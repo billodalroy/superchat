d6064e16ce901875cc17998aaa8576826e777670
Billodal Roy <billodalroy@gmail.com>
2022-11-29 03:01:14 -0500

    build

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# superchat
+# SuperChat App
+
+- This app is Built with React and Firebase.
+- It mimics the functionality of Youtube Superchat.
+- the App supports multiple authentication options and realtime chat along with content moderation.
---
+++ build/precache-manifest.982486ed07f09fe4282de66ba5fa874f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "501a72cdc602a04bb266",
+    "url": "/static/css/main.8b7b8323.chunk.css"
+  },
+  {
+    "revision": "501a72cdc602a04bb266",
+    "url": "/static/js/main.501a72cd.chunk.js"
+  },
+  {
+    "revision": "8a5b0b70b12187e5da28",
+    "url": "/static/js/1.8a5b0b70.chunk.js"
+  },
+  {
+    "revision": "f9938fa27aa7fac1f413",
+    "url": "/static/js/2.f9938fa2.chunk.js"
+  },
+  {
+    "revision": "a46ef3f558368343807d",
+    "url": "/static/js/runtime~main.a46ef3f5.js"
+  },
+  {
+    "revision": "6558d3aaed730cb32b13133ccbba1263",
+    "url": "/index.html"
+  }
+];
